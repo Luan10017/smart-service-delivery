972defa868c0701a5579de60455927a64fe44722
Luan <luan10017@hotmail.com>
2021-10-27 22:29:51 -0300

    Inicializando projeto e configurando bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as jQuery from 'jquery'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smart-service';
+
+  ngAfterViewInit(){
+    (function($) {
+        "use strict";
+    
+        // Add active state to sidbar nav links
+        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+                if (this.href === path) {
+                    $(this).addClass("active");
+                }
+            });
+    
+        // Toggle the side navigation
+        $("#sidebarToggle").on("click", function(e) {
+            e.preventDefault();
+            $("body").toggleClass("sb-sidenav-toggled");
+        });
+    })(jQuery);
+  }
+
 }
