d3931b6378b6a3f80c1587baa63c91cc49e9493e
AndreCypriano23 <andrecypriano23@outlook.com>
2021-10-31 20:01:21 -0300

    Componentização e desenvolvimento das primeiras telas(navbar, sidebar e apresentação)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,15 +13,15 @@ export class AppComponent {
   ngAfterViewInit(){
     (function($) {
         "use strict";
-    
+
         // Add active state to sidbar nav links
         var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
             $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-                if (this.href === path) {
+                /*if (this.href === path) {
                     $(this).addClass("active");
-                }
+                }*/
             });
-    
+
         // Toggle the side navigation
         $("#sidebarToggle").on("click", function(e) {
             e.preventDefault();
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { PresentationComponent } from './presentation/presentation.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    PresentationComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    PresentationComponent
   ]
 })
 export class TemplateModule { }
