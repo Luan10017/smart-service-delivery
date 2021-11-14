59a0010f05120b25414bd3542cba1cfd3d9bdad1
AndreCypriano23 <andrecypriano23@outlook.com>
2021-11-14 19:16:27 -0300

    Primeira versão de outras telas do template desenvolvidas, cadastro, mesa, pagamento, pedido, produto e  endereco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
 import { LoginComponent } from './navigation/login/login.component';
 import { CadastroComponent } from './navigation/cadastro/cadastro.component';
+import { ProdutoComponent } from './navigation/produto/produto.component';
+import { MesaComponent } from './navigation/mesa/mesa.component';
+import { PagamentoComponent } from './navigation/pagamento/pagamento.component';
+import { EnderecoComponent } from './navigation/endereco/endereco.component';
+import { PedidoComponent } from './navigation/pedido/pedido.component';
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent,  },
   { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'produto', component: ProdutoComponent },
+  { path: 'mesa', component: MesaComponent },
+  { path: 'pagamento', component: PagamentoComponent },
+  { path: 'endereco', component: EnderecoComponent },
+  { path: 'pedido', component: PedidoComponent }
 ];
 
 @NgModule({
---
+++ src/app/navigation/mesa/mesa.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mesa',
+  templateUrl: './mesa.component.html',
+  styleUrls: ['./mesa.component.css']
+})
+export class MesaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
