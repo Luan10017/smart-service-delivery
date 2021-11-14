8f1ea945faf20eaa3f901f2986f72d9660311e59
Luan <luan10017@hotmail.com>
2021-11-13 21:37:15 -0300

    Add método para buscar products na API

---
+++ src/app/menu.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Product } from './components/product-card/product.model'
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  baseUrl = "http://localhost:3001/products"
+  constructor(private http: HttpClient) { }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+}
