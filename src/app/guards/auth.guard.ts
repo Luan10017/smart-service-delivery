2a56a72439a783f7f942cadd22689845487427b4
Luan <luan10017@hotmail.com>
2021-12-05 22:26:08 -0300

    add guard para login

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
+
+import { AuthService } from './../view/login/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ) : Observable<boolean> | boolean {
+
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso(){
+    if (this.authService.usuarioEstaAutenticado()){
+      return true;
+    } 
+
+    this.router.navigate(['/login']);
+
+    return false;
+  }
+
+}
