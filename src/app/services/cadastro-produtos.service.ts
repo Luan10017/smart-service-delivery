ee6a3fe6b2eb23cce45900dffa3190df7ee22808
Luan10017 <luan10017@hotmail.com>
2022-01-16 22:49:39 -0300

    cadastro de produtos

--- src/app/services/cadastro-produtos.service.ts
+++ src/app/services/cadastro-produtos.service.ts
@@ -2,6 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+
+export interface ObjetoPayload {
+  data: Array<ObjetoPayload2<string>>;
+}
+
+export interface ObjetoPayload2<T> {
+  produtoId: Array<string>;
+ }
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,9 +20,12 @@ export class CadastroProdutosService {
 
   constructor(private http: HttpClient) { }
 
-  ok(cadastro: any): Observable<any> {
-    return this.http.post<any>(this.baseUrl,cadastro)
+  ok(cadastro: any): Observable<ObjetoPayload> {
+    return this.http.post<ObjetoPayload>(this.baseUrl,cadastro)
   }
+  // ok(cadastro: any): Observable<any> {
+  //   return this.http.post<any>(this.baseUrl,cadastro)
+  // }
 
   
   putItem(item: any, id: any) : Observable<any> {
