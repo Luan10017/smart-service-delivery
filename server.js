1df6b76664c042f131aeee85fe6dc606db717fda
luan10017 <luan10017@hotmail.com>
2022-04-23 11:50:22 -0300

    Deploy no heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/dist/smart-service'))
+
+app.get('/*', (req,res) => {
+    res.sendFile(__dirname + '/dist/smart-service/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT)
+})
