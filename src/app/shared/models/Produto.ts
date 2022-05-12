af7b9de54d5bcc9afbb962107a07e4dd11db185b
luan10017 <luan10017@hotmail.com>
2022-05-12 17:10:26 -0300

    corrigi model da categoria

--- src/app/shared/models/Produto.ts
+++ src/app/shared/models/Produto.ts
@@ -1,9 +1,14 @@
 export class Produto {
     id!: string
-    categoria!: string 
+    categoria!: Categoria 
     nome!: string 
     preco!: number
     descricao!: string
     estoque!: number
     imgUrl!: string
+}
+
+export class Categoria {
+    id!: string
+    nome!: string
 }
